464ec5fc9fe6652598b8cff12638c436eb70dde6
qhxin <qhxin@users.noreply.github.com>
2022-12-01 01:29:19 +0800

    sw

--- src/index.js
+++ src/index.js
@@ -13,3 +13,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
